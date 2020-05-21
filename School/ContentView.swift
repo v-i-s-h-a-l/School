ee469d810b76deccf448bd1ce17bf1bf6cb3aeeb
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2020-05-22 04:12:35 +0530

    passing data on multi level

--- School/ContentView.swift
+++ School/ContentView.swift
@@ -9,6 +9,12 @@
 import Combine
 import SwiftUI
 
+extension View {
+    func endEditing() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
 class Student: ObservableObject, Identifiable {
     let id = UUID()
     @Published var name: String = ""
@@ -22,30 +28,34 @@ class Student: ObservableObject, Identifiable {
 
 class Standard: ObservableObject, Identifiable {
     let id = UUID()
-    @Published var name: String = ""
+    var name: String = ""
     @Published var students: [Student] = []
     @Published var averageMarks: String = ""
     
-    private var cancellable: AnyCancellable?
+    private var cancellables = [AnyCancellable]()
     
-    var studentCountPublisher = PassthroughSubject<Int, Never>()
-
+    var studentCountUpdatedPublisher = PassthroughSubject<Void, Never>()
+    
     func createNewStudent() {
         let newStudent = Student(students.count + 1, marks: "0.0")
-        cancellable = newStudent.objectWillChange
-            .debounce(for: 0.1, scheduler: DispatchQueue.main, options: nil)
-            .sink { _ in
-            self.updateAverage()
-        }
+        admitStudent(newStudent)
+        studentCountUpdatedPublisher.send(())
+    }
+
+    private func admitStudent(_ newStudent: Student) {
         students.append(newStudent)
+        let newStudentSubscription = newStudent
+            .objectWillChange
+            .debounce(for: 0.1, scheduler: DispatchQueue.main, options: nil)
+            .sink(receiveCompletion: { _ in
+                self.updateAverage()
+            }, receiveValue: { _ in })
+        cancellables.append(newStudentSubscription)
     }
 
     private func updateAverage() {
-        let sum = students.reduce(0.0) { sum, student in
-            return sum + (Double(student.marks) ?? 0.0)
-        }
+        let sum = students.reduce(0.0) { $0 + (Double($1.marks) ?? 0.0) }
         self.averageMarks = String(format: "%.2f", (sum / Double(students.count)))
-        print(cancellable == nil)
     }
 
     init(_ number: Int) {
@@ -54,49 +64,56 @@ class Standard: ObservableObject, Identifiable {
 }
 
 class VSSchool: ObservableObject, Identifiable {
-    
-    typealias Input = Int
-    typealias Failure = Never
-    
     let id = UUID()
 
-    @Published var name: String = "SCHOOL"
+    var name: String = "SCHOOL"
     @Published var classes: [Standard] = []
+    @Published var totalStudents: Int = 0
+
+    private var cancellables = [AnyCancellable]()
 
     func createNewStandard() {
         let newStandard = Standard(classes.count + 1)
+        subscribeToStudentsCount(for: newStandard)
         classes.append(newStandard)
-//        newStandard.objectWillChange.receive(subscriber: <#T##Subscriber#>)
-        objectWillChange.send()
+    }
+    
+    private func subscribeToStudentsCount(for newStandard: Standard) {
+        let cancellable = newStandard
+            .studentCountUpdatedPublisher
+            .print("Student count updater in school: \(newStandard.name)")
+            .sink(receiveValue: { _ in
+                self.totalStudents += 1
+            })
+        self.cancellables.append(cancellable)
     }
 }
 
-
 struct SchoolView: View {
-
+    
     @ObservedObject var school: VSSchool
-
+    
     var body: some View {
-        return HStack {
-            Spacer()
             VStack {
-                Text(school.name)
+                Text(self.school.name)
+                    .fontWeight(Font.Weight.heavy)
                     .font(.largeTitle)
+                    .rotation3DEffect(Angle.degrees(-Double(45)), axis: (x: 1, y:0, z: 0))
+                    .shadow(color: Color.black, radius: 6, x: 0, y: 10)
                     .padding()
-                Text("Total Classes: \(school.classes.count)")
+                
+                Text("Total Students: \(self.school.totalStudents)")
                     .font(.headline)
                     .padding([.bottom])
-    //            Text("Total students: \(school.totalStudents)")
-    //                .font(.footnote)
-    //                .padding([.bottom])
-                EditSchoolView(school: school)
+                //            Text("Total students: \(school.totalStudents)")
+                //                .font(.footnote)
+                //                .padding([.bottom])
+                EditSchoolView(school: self.school)
                     .padding([.bottom])
                 VStack {
-                    ForEach(school.classes, content: StandardView.init)
+                    ForEach(self.school.classes, content: StandardView.init)
                 }
             }
-            Spacer()
-        }
     }
 }
 
@@ -120,12 +137,14 @@ struct StandardView: View {
                 .font(.title)
                 .padding()
             HStack {
+                Spacer()
                 Text("Total students: \(standard.students.count)")
                     .font(.subheadline)
                     .padding()
                 Text("Avg. Marks: \(standard.averageMarks)")
                     .font(.subheadline)
                     .padding()
+                Spacer()
             }
 
             EditClassView(standard: standard)
@@ -168,31 +187,33 @@ struct EditStudentView: View {
 
     var body: some View {
         HStack {
+            Spacer()
             TextField("Student Name", text: $student.name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
                 .foregroundColor(Color.primary)
+                .padding([.trailing])
             TextField("Marks", text: $student.marks)
-            .foregroundColor(Color.primary)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .foregroundColor(Color.primary)
+                .padding([.trailing])
+            Spacer()
         }
     }
 }
-
 struct ContentView: View {
     
+    @ObservedObject var school = VSSchool()
+    
     var body: some View {
-        let standard = Standard(1)
-
-        let school = VSSchool()
-        school.classes.append(standard)
-        
-        return ScrollView {
-            Spacer()
+        ScrollView {
             SchoolView(school: school)
+                .onTapGesture {
+                    self.endEditing() }
+                .background(
+                    Color.purple
+                        .edgesIgnoringSafeArea(.all))
+                .accentColor(.orange)
         }
-        .background(
-            Color.purple
-            .edgesIgnoringSafeArea(.all)
-        )
-        .accentColor(.orange)
     }
 }
 
@@ -200,9 +221,11 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
+            Group {
             ContentView().colorScheme(.dark)
+                .previewDevice(PreviewDevice.init(rawValue: "iPhone SE"))
             ContentView().colorScheme(.light)
+                .previewDevice(PreviewDevice.init(rawValue: "iPhone 6S"))
         }
     }
 }
